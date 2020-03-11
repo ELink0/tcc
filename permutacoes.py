7444a45d56af95dcb00aef28de1e998b4ff07730
Link0 <36975653+ELink0@users.noreply.github.com>
2020-03-11 19:23:19 -0300

    Update permutacoes.py

--- permutacoes.py
+++ permutacoes.py
@@ -1,21 +1,24 @@
-lista = ['a', 'b', 'c']
+matriz_adjs = [['a', 'b', 'c'], ['0', '1', '3', '4']]
 lista1 = ''
 k = 0
 
-def permuta(lista, lista1, z):
+def permuta(matriz, lista1, z):
 	global k
-	if len(lista) == z:
-	    # assim que o processo do FOR abaixo for finalizado, será imprimido aqui 
-	    # os elementos da lista1, que são os que foram permutados
-		print k, lista1
+	if len(matriz) == z:
+		# assim que o processo do FOR abaixo for finalizado, sera imprimido aqui 
+		# os elementos da lista1, que so os que foram permutados
+		print k, ' ', lista1
 		k += 1
 		
 	else:
-	    # Nesta parte nós vamos estar percorrendo a nossa matriz
-	    # e a cada iteração do for vamos estar adicionando o elemento i 
-	    # na lista1
-	    # para cada elemento percorrido da lista vai ser adicionado ao lista1
-		for i in range(len(lista)):
-			permuta(lista, lista1+lista[i], z+1)
+		# Nesta parte nos vamos estar percorrendo a nossa matriz
+		# e a cada iteracao do for vamos estar adicionando o elemento i 
+		# na lista1
+		# para cada elemento percorrido da lista vai ser adicionado ao lista1
+		for i in range(len(matriz[z])):
+			permuta(matriz, lista1+' '+matriz[z][i], z+1)
         
-permuta(lista, lista1, 0)
+permuta(matriz_adjs, lista1, 0)
+
+
+# o prof eduardo eh <t0> e o sei la o que eh <t1>
